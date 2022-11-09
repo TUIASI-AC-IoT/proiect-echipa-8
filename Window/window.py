42e87d2e74db84944d8e0555779287a55619ca8d
florinnis3 <florin.nistor@student.tuiasi.ro>
2022-11-09 19:57:10 +0200

    InterfataDePornire

---
+++ Window/window.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+def startWindow():
+    window = Tk()
+    window.title("SNMP interface")
+    window.configure(width=500, height=500)
+    window.configure(bg='lightgrey')
+    window.mainloop()
