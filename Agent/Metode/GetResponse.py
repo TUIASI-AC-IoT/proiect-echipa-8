b2e72ef7775f2bf3b4e0d106a77d3d5035148780
FlorinNis <florin.nistor@student.tuiasi.ro>
2022-12-06 22:22:00 +0200

    Conexiune Agent Interfata(GetRequest)

--- Agent/Metode/GetResponse.py
+++ Agent/Metode/GetResponse.py
@@ -1,4 +1,20 @@
 import socket
 
-def GetResponseRun(s):
-    s.sendall(bytes("GetRequest", "utf-8"))
+def GetRequestRun():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((socket.gethostname(), 161))
+    s.listen(1)
+    print('Asteapta Conexiuni')
+    conn, addr = s.accept()
+    print('Conectat la', addr)
+    while 1:
+        data = conn.recv(1024)
+        if not data: break
+        print(data)
+        if (data == b'Temperatura'):
+            # oid: 1.127.10.5.6
+            conn.sendall(bytes("Temperatura: 25", "utf-8"))
+
+        else:
+            conn.sendall(bytes("Invalid", "utf-8"))
+    conn.close()
